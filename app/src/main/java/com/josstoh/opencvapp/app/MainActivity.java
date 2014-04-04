feb25e22e4166bf55e7243cba32827b06e569dbb
Allyaenn <lilixfrom01@hotmail.fr>
2014-04-04 10:23:38 +0200

    Je teste une deuxième fois

--- app/src/main/java/com/josstoh/opencvapp/app/MainActivity.java
+++ app/src/main/java/com/josstoh/opencvapp/app/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends ActionBarActivity implements CvCameraViewListe
     private CameraBridgeViewBase mOpenCvCameraView;
     private String TAG = "OpenCVApp";
 
- //test
+    //test
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
